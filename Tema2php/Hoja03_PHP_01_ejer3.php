6eb6c26b3029f265b1909f1fb944aee742f50052
p1t0n83 <rompeolas93@gmail.com>
2024-09-25 12:16:32 +0200

    finalizacion del ejercicio 3

--- Tema2php/Hoja03_PHP_01_ejer3.php
+++ Tema2php/Hoja03_PHP_01_ejer3.php
@@ -7,14 +7,26 @@
 </head>
 <body>
     <?php
-    $numero=12345;
-    //casteamos el numero a string
-    //con strlen podemos ver la longitud de un string
-    $longitud=strlen((string)$numero);
+    // a ver si no me lio xd
+    $numero='22322';
+    //con strlen podemos ver la longitud de un string,hice el numero String porque es mas facil trabajar con el así
+    $longitud=strlen($numero);
+    //creamos una variable para almacenar si es capicua o no
+    $capicua=true;
+    // hacemos un for que llegue hasta la mitad
     for($i=0;$i<=$longitud/2;$i++){
-    
+        // hacemos dos variables, una que tome el valor de la posicion que va aumentando
+        //y la otra la contraria
+        $numero1=$numero[$i];
+        //para ello sacamos la longitud del numnreo,le restamos 1 y le restamos la posicion del primer valor
+        $numero2=$numero[strlen($numero)-1-$i];
+        //con el if comprobamos si son iguales los dos numeros y si no lo son capicua toma un valor falso
+    if($numero1!=$numero2){
+    $capicua=false;
     }
-    echo "$longitud";
+    }
+    //un ternario,usalos cabron
+    echo $capicua ? 'Sí, es capicúa' : 'No, no es capicúa';
     ?>
 </body>
 </html>
