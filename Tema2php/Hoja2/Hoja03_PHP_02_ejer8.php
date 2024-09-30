44ba9ec11e18c2ee34f5397d6ffe12624d8c1d07
p1t0n83 <rompeolas93@gmail.com>
2024-09-30 19:34:25 +0200

    simplemente he comentado el ejercicio 8

--- Tema2php/Hoja2/Hoja03_PHP_02_ejer8.php
+++ Tema2php/Hoja2/Hoja03_PHP_02_ejer8.php
@@ -8,30 +8,42 @@
 <body>
     <?php
     function trabajoCadenas($cadena){
+        //strpos nos permite buscar una cadena en especifico dentro de otra cadena
         $posicion=strpos($cadena,"algas");
     echo 'Posicion de la palabra "algas":'.$posicion. '<br/>';
-    
+
+
+    //str_replace nos permite sustituir una parte de una cadena por otra
     $cadena=str_replace("realmente", "muy", $cadena);
     echo $cadena.'<br/>';
 
-     if(strpos($cadena,"anacardo")==false){
+
+     //lo mismo de antes,buscamos si esta anacardo
+     if(strpos($cadena,"anacardo")===false){
      echo "no existe la palabra anacardo <br/>";
      }else{
      echo "Existe la palabra anacardo <br/>";
      }
 
+
+       //strrev nos permite invertir una cadena
      $cadenaInvertida = strrev($cadena);
      echo $cadenaInvertida.'<br/>';
 
+
+    //miramos posicion a posicion de la cadena mediante un for para encontrar cuantas 'e' ahí
     $contador=0;
     for ($i = 0; $i < strlen($cadena); $i++) {     
         if ($cadena[$i] === 'e') {
             $contador++;
         }
     }
+
+
     echo "La cantidad de 'e' en la cadena es: $contador <br/>";
     }
     
+
     trabajoCadenas('Comer algas es realmente sano');
 
 
