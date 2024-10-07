128043ffc383914a7a8fd77eb776517111f95ede
p1t0n83 <rompeolas93@gmail.com>
2024-10-07 20:02:19 +0200

    commit de las contraseñas

--- Tema2php/Hoja1/vendor/composer/autoload_psr4.php
+++ Tema2php/Hoja1/vendor/composer/autoload_psr4.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Hackzilla\\PasswordGenerator\\' => array($vendorDir . '/hackzilla/password-generator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
--- Tema2php/Hoja1/vendor/composer/autoload_real.php
+++ Tema2php/Hoja1/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitdc73f7577b3cc87c682c6b6f49ee27af
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitdc73f7577b3cc87c682c6b6f49ee27af::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
