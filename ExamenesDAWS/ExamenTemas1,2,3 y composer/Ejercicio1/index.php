552cefb92c27798c821608c4a434de23f1b0c6de
p1t0n83 <rompeolas93@gmail.com>
2024-10-25 20:09:17 +0200

    falta una cosa, y es que en  el ejercicio 1 no he añadido el hidden para que se mantengan los datos

--- ExamenesDAWS/ExamenTemas1,2,3 y composer/Ejercicio1/index.php
+++ ExamenesDAWS/ExamenTemas1,2,3 y composer/Ejercicio1/index.php
@@ -12,6 +12,7 @@
 <body>
   <form action="" method="get">
     <h1> Gestion de Contactos</h1>
+    
     <label for="nombre">Nombre:</label>
     <input type="text" name="nombre" id="nombre" required> <br>
     <label for="correo">Correo Electronico:</label>
@@ -22,6 +23,7 @@
   </form>
 
   <?php
+  require_once "validaciones.php";
   $contactos = array(
     "Contacto1" => array(
       "Nombre" => "Juan Pérez",
@@ -42,21 +44,46 @@
       $nombre = $_GET['nombre'];
       $correo = $_GET['correo'];
       $telefono = $_GET['telefono'];
-      // Agregamos el nuevo contacto al array
-      $contactos[] = array(
-        "Nombre" => $nombre,
-        "Email" => $correo,
-        "Telefono" => $telefono
-      );
+
+      $mensaje=" ";
+      if(validarNombre($nombre)){
+        if(validarEmail($correo)){
+          if(validarTelefono($telefono)){
+            $mensaje = "se ha añadido de manera satisfactoria ";
+              // Agregamos el nuevo contacto al array
+              $contactos[] =array(
+                "Nombre" => $nombre,
+                "Email" => $correo,
+                "Telefono" => $telefono
+              );
+          }else{
+            $mensaje="El telefono esta mal OnO";
+          }
+        }else{
+          $mensaje= "El correo esta malito";
+        }
+      }else{
+        $mensaje="El nombre es invalido";
+      }
+
+    
+
     }
+    ?>
+
+
+
+    <?php
+
+    echo $mensaje;
     // la tabla
     echo "<table border='1'";
     echo "<tr><th>Nombre</th><th>Email</th><th>Telefono</th></tr>";
     foreach ($contactos as $contacto) {
       echo "<tr>";
-      echo "<td>" . htmlspecialchars($contacto["Nombre"]) . "</td>";
-      echo "<td>" . htmlspecialchars($contacto["Email"]) . "</td>";
-      echo "<td>" . htmlspecialchars($contacto["Telefono"]) . "</td>";
+      echo "<td>" . $contacto["Nombre"]. "</td>";
+      echo "<td>" . $contacto["Email"] . "</td>";
+      echo "<td>" . $contacto["Telefono"] . "</td>";
       echo "</tr>";
     }
     echo "</table>";
--- ExamenesDAWS/ExamenTemas1,2,3 y composer/Ejercicio1/validaciones.php
+++ ExamenesDAWS/ExamenTemas1,2,3 y composer/Ejercicio1/validaciones.php
@@ -1,14 +1,27 @@
 <?php
 
-function validarNombre(){
-
+function validarNombre($nombre){
+    if(strlen($nombre)>=3){
+    return true;
+    }else{
+        return false;
+    }
 }
 
-function validarEmail(){
-
+function validarEmail($correo){
+    if(filter_var($correo,FILTER_VALIDATE_EMAIL)){
+        return true;
+    }else{
+        return false;
+    }
 }
 
-function validarTelefono(){
-    
+function validarTelefono($numero){
+if(preg_match('/^[0-9]{9}/',$numero)){
+    return true;
+}   else{
+    return false;
+}
+
 }
 ?>
