ddc37b1023a97676cdd46bf0d89d0854f51a1839
p1t0n83 <rompeolas93@gmail.com>
2024-11-04 20:05:21 +0100

    conexion completa

---
+++ Tema4php/Hoja1/Ejercicio1/public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require_once '../vendor/autoload.php';
+    use App\Clases\ConexionBD;
+    $connection = ConexionBD::getConnection();
+
+    if ($connection instanceof PDO) {
+        echo 'Conexión establecida correctamente';
+    }
+
+
+    ?>
+</body>
+
+</html>
