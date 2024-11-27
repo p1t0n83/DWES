f482b7aa6547417d784800e522cff20c95fc173b
p1t0n83 <rompeolas93@gmail.com>
2024-11-27 18:59:08 +0100

    es para confirma que se me guarda

---
+++ Tema5php/Hoja1/public/Paginas/Registro.php
@@ -0,0 +1,29 @@
+<?php
+require '../../vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <h1>Registro de usuario</h1>
+        <hr>
+        <label for="nombre"><input type="text" id="nombre" name="nombre" placeholder="Su nombre"></label>
+        <hr>
+        <label for="password"><input type="text" id="password" name="password" placeholder="Su clave"></label>
+        <hr>
+        <label for="passwordre"><input type="text" id="passwordre" name="passwordre" placeholder="Su clave"></label>
+        <hr>
+    </form>
+    <?php
+
+    ?>
+</body>
+
+</html>
