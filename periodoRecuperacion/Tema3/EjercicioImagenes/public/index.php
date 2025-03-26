0f88b531e935972b31ae6ed64bffb131594cea89
p1t0n83 <rompeolas93@gmail.com>
2025-03-26 20:14:33 +0100

    df

---
+++ periodoRecuperacion/Tema3/EjercicioImagenes/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Cambia nombres de imagenes</h1>
+    <form>
+        <label for="nombre">Escriba el nuevo nombre del archivo
+    </form>
+</body>
+
+</html>
