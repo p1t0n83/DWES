2b7a6cc4f9ec5654da306ad214bc01dee8778747
p1t0n83 <rompeolas93@gmail.com>
2025-04-01 23:45:15 +0200

    si

--- periodoRecuperacion/Tema3/EjercicioImagenes/public/index.php
+++ periodoRecuperacion/Tema3/EjercicioImagenes/public/index.php
@@ -8,10 +8,91 @@
 </head>
 
 <body>
-    <h1>Cambia nombres de imagenes</h1>
-    <form>
-        <label for="nombre">Escriba el nuevo nombre del archivo
+    <h1>Albumnes</h1>
+    <form enctype="multipart/form-data" method="post" action="procesa.php">
+        Nombre del album:<br>
+        <label for="nombre"><input type="text" name="nombre" placeholder="titulo del album" required></label><br>
+        Imagen:<br>
+        <label for="imagen"><input type="file" name="imagen" required> </label><br>
+        <br>
+        <button>Enviar album</button>
     </form>
+
+    <?php
+    $mensaje = '';
+
+    /**
+     * Miro si el get me devuelve un error
+     */
+    if (isset($_GET["error"])) {
+        $error = intval($_GET["error"]);
+
+        /**
+         * Guardo un mensaje personalizado para cada tipo de error
+         */
+        $mensaje = match ($error) {
+            1 => isset($_GET["mensaje"]) ? "<p class='error'>".$_GET["mensaje"]."</p>" : '<p class="error">Ha ocurrido un error en la imagen</p>',
+            2 => '<p class="error">La extensión del fichero no es válida, debe ser jpg, png o pdf</p>',
+            3 => '<p class="error">El directorio imagenes no existe</p>',
+            4=>'<p class=""error>No se ha podido mover el fichero</p>',
+            default => '<p class="error">Error inesperado</p>'
+        };
+    }
+
+    /**
+     * Compruebo si el get me devuelve un success 
+     */
+    if (isset($_GET["success"])) {
+
+        /**
+         * Escanea el directorio que pase por parámetro y me devuelve un array con su contenido
+         */
+        $imagenes=scandir("imagenes/");
+
+        foreach($imagenes as $imagen){
+
+            /**
+             * Elimino que no me muestre el . y .. ya que me lo devuelve el scandir
+             */
+            if($imagen !== '.'  && $imagen!=='..'){
+
+                /**
+                 * Obtengo la información del fichero
+                 */
+                $infoFichero=pathinfo($imagen);
+
+                /**
+                 * Obtengo su extensión
+                 */
+                $extension=$infoFichero["extension"];
+
+                /**
+                 * Obtengo el nombre del fichero
+                 */
+                $nombreFichero=$infoFichero["filename"];
+        
+                /**
+                 * Si el fichero tiene extension pdf, me mostrará un link para pinchar sobre el y  se abrirá una nueva pestaña con el fichero pdf
+                 * Si es una imagen, se mostrará un titulo con el nombre de la imagen y la foto correspondiente
+                 */
+                if($extension === "pdf" ){
+                    echo "<p><a href='imagenes/" . $infoFichero['basename'] . "' target='_blank'>Ver PDF". $infoFichero["filename"]. "</a></p>";
+                }else{
+                    echo "<h1>". $nombreFichero. "</h1>";
+                    echo "<img src='imagenes/" . $infoFichero['basename'] . "' alt='$infoFichero[filename]'>";
+                }
+            }
+        }
+    }
+
+    /**
+     * Si hay un mensaje, se muestra
+     */
+    if ($mensaje) {
+        echo $mensaje;
+    }
+?>
+
 </body>
 
 </html>
---
+++ periodoRecuperacion/Tema3/EjercicioImagenes/public/procesa.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Guardo los datos
+ */
+$imagen = $_FILES['imagen'];
+$nombreNuevo = $_POST['nombre'];
+
+$tipos_permitidos = [
+    "image/jpeg" => "jpeg",
+    "image/jpg" => "jpg",
+    "image/png" => "png",
+    "application/pdf" => "pdf"
+];
+/**
+ * Si ocurre algún error durante la subida del fichero, saltará un error=2
+ * 0 significa que no ha habido ningún error
+ */
+if ($fichero["error"] !== 0) {
+
+    /**
+     * Recojo el código del error
+     */
+    $codigoError = $fichero["error"];
+
+    /**
+     * Dependiendo del codigo de error que aparezca, mostraré un mensaje
+     */
+    $mensajeError = match ($codigoError) {
+        1 => 'El archivo cargado excede la directiva upload_max_filesize en php.ini',
+        2 => 'El archivo cargado excede la directiva MAX_FILE_SIZE que se especificó en el formulario HTML',
+        3 => 'El archivo cargado solo se cargó parcialmente',
+        4 => 'No se cargó ningún archivo',
+        6 => 'Falta una carpeta temporal',
+        7 => 'No se pudo escribir el archivo en el disco.',
+        8 => 'Una extensión PHP detuvo la carga del archivo.',
+        default => 'Codigo inesperado'
+    };
+
+    header("Location:index.php?error=1&mensaje=" . $mensajeError);
+    exit();
+}
+
+
+/** valido el tipo de file**/
+if (!array_key_exists($fichero['type'], $tipos_permitidos)) {
+    header("Location:index.php?error=2");
+    exit;
+}
+
+/**cambimos el nombre del archivo si existe */
+if(!empty($nombreNuevo)){
+    $fichero['name']=$nombreNuevo . "." . $tipos_permitidos[$fichero["type"]];
+}
+
+ /**
+     * Guardo en una variable el directorio donde moveré la imagen
+     */
+    $directorioDestino = './imagenes/';
+
+     /**
+     * Compruebo si existe el directorio imagenes
+     * Si no existe, mostrará el mensaje de error 3
+     */
+    if(!file_exists($directorioDestino)){
+        header("Location:index.php?error=3");
+        exit();
+    }
+ /**Creo la ruta donde se moverá la imagen */
+ $ubicacionFinal = $directorioDestino . basename($fichero["name"]);
+
+/* guardamos el archivo en la nueva ruta*/ 
+ if(!move_uploaded_file($fichero["tmp_name"], $ubicacionFinal)){
+      header('Location:index.php?error=4');
+      exit;
+ }else{
+    header("Location:index.php?success=1");
+    exit;
+ }
+
+?>
